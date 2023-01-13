64f64b6a172da625c79ef18c3b74abe90cd3e832
Daniela <109166579+danidani0143@users.noreply.github.com>
2023-01-13 12:18:48 -0600

    agregé javascript
    tuve que hacer el cambio de manera manual porque con la consola me sale un error :( tengo que checar por qué

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,6 +1,9 @@
-// API
-const API_ENDPOINT = 'https://yesno.wtf/api';
 
+const API_ENDPOINT = 'https://yesno.wtf/api';
+const btn = document.querySelector('#button');
+const resEl = document.querySelector('#answer');
+const inputEl = document.querySelector('#input');
+const answer = [];
 /**
  * STEPS:
  *
@@ -11,3 +14,27 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+
+const fetchAnswer = async function () {
+    const response = await fetch(API_ENDPOINT);
+    const data = response.json();
+    return data;
+};
+
+fetchAnswer().then((resvolve, reject) => console.log(answer.push(resvolve), reject));
+
+btn.addEventListener('click', e => {
+    e.preventDefault();
+    if (!inputEl.value) return;
+    resEl.innerHTML = '';
+    fetchAnswer().then((resvolve, reject) => answer.push(resvolve));
+    resEl.insertAdjacentHTML(
+        'beforeend',
+        `
+      <span>
+        ${answer[0].answer}
+      </span>
+    `
+    );
+    answer.length = 0;
+});
